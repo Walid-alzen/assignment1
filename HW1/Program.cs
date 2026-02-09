d05e0de30339ec0007aef04a55a5b5b63b89d571
Waleed alzen <Waleedzen2005@gmail.com>
2026-02-09 18:17:12 +0300

    Add project files.

---
+++ HW1/Program.cs
@@ -0,0 +1,43 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        char tryAgain;
+        do
+        {
+            Console.Write("enter the payment amount : ");
+            double paymentAmount = Convert.ToDouble(Console.ReadLine());
+            double paymentAfterDiscount = paymentAmount;
+            if (paymentAmount >= 500)
+            {
+                paymentAfterDiscount = paymentAfterDiscount - paymentAmount * 0.2 ;
+            }
+            else if (paymentAmount >= 300 && paymentAmount < 500)
+            {
+                paymentAfterDiscount = paymentAfterDiscount -  paymentAmount * 0.1  ;
+            }
+            else if (paymentAmount < 300)
+            {
+                paymentAfterDiscount = paymentAmount;
+            }
+            DateTime paymentTime = DateTime.Now;
+            string payment_Time = paymentTime.ToString("dd-MM-yyyy hh:mm:ss tt");
+            Guid paymentId = Guid.NewGuid();
+
+            Console.WriteLine($"the final price is : {paymentAfterDiscount} \npayment time : {payment_Time}");
+            Console.WriteLine($"your bill Id {paymentId} ");
+            Console.Write($"do you have another payment y/n ? ");
+          
+             tryAgain = Convert.ToChar( Console.ReadLine());
+         
+        }
+        while (tryAgain == 'y');
+        /*
+         * i faced a problem int the tryAgain var 
+         * the var was inside the (do) loop thats why it didnt work
+         * i asked shatgpt to fix and explain it 
+         * thats the only AI help i used 
+         *
+         */
+    }
+}
